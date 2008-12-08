74b4892d149c0cd64cefec755eae867f6dab2e52
unknown <jflam@.redmond.corp.microsoft.com>
2008-12-08 13:16:11 -0800

    finished first part of lab

--- tutorial.rb
+++ tutorial.rb
@@ -1,13 +1,11 @@
-p "For each of the forty largest countries in the world (according to 1990
-population figures), data are given for the country's life expectancy
-at birth, number of people per television set, and number of people per
-physician."
+p "For each of the forty largest countries in the world (according to 1990 population figures), data are given for the country's life expectancy at birth, number of people per television set, and number of people per physician."
 
 p "Your job is to write a program that determines the correlation, if any, between these data. Your program should use the Sho libraries where possible, and you should work with a partner to finish this assignment. One way of breaking up the work is to have on of you work on the graphical elements, and have the other work on analyzing the data."
 
 p "The data set for this assignment is already loaded into the Live Mesh for the class. There, you'll find a file called televisions.csv that contains the data for this assignment. You can read the data into a local matrix usig this command:"
 
 code(__LINE__) {
+require 'sho'
 m = read_csv_matrix Mesh.open('sho/televisions.csv')
 }
 
@@ -22,13 +20,13 @@ drs = m.GetCol(3)
 p "The first thing that you might want to do is plot tvs vs. life:"
 
 code(__LINE__) {
-chart = show_scatter(tvs, life)
+show_scatter(tvs, life)
 }
 
 p "You'll see that there isn't a particularly good correlation there. So let's plot the log of tvs vs. life:"
 
 code(__LINE__) {
-chart.plot_points(log(tvs), life)
+chart = show_scatter(log(tvs), life)
 }
 
 p "That looks better. Let's see what it looks like to fit a least-squares regression line against that data:"
@@ -42,6 +40,7 @@ chart.plot_straight_line drange(0, 7, 1), slope, intercept
 p "Let's do the same thing for the doctor data:"
 
 code(__LINE__) {
+chart.plot_points log(drs), life
 r2 = Regress.new life, log(drs)
 intercept, slope = r2.Beta.first, r2.Beta.last
 chart.plot_straight_line drange(0, 11, 1), slope, intercept
@@ -55,5 +54,11 @@ puts "tvs: #{r1.Rsq} doctors: #{r2.Rsq}"
 
 p "What conclusions can you draw from this data?"
 
-p "Student 2"
+p "Now, let's switch to the other student."
+
+p "Finally, let's pull the code from the other student and test run it in our application."
+
+code(__LINE__) {
+pull 'jflam42 master'
+}
 
