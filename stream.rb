2ba3c06b2cf6f69fe8e576df49c0ab237d9f0278
unknown <Steve@.redmond.corp.microsoft.com>
2008-12-10 15:27:15 -0800

    a method

---
+++ stream.rb
@@ -0,0 +1,37 @@
+
+require 'PresentationFramework'
+require 'WindowsFormsIntegration'
+
+include System::Windows
+include System::Windows::Controls
+include System::Windows::Forms::Integration
+
+def create_winforms_floating_window(control)
+  w = Window.new
+  w.Topmost = true
+  w.WindowStyle = WindowStyle.ToolWindow
+  host = WindowsFormsHost.new
+  host.child = control
+  w.Content = host
+  w
+end
+
+
+require 'PresentationFramework'
+require 'WindowsFormsIntegration'
+
+include System::Windows
+include System::Windows::Controls
+include System::Windows::Forms::Integration
+
+def create_winforms_floating_window(control)
+  w = Window.new
+  w.Topmost = true
+  w.WindowStyle = WindowStyle.ToolWindow
+  host = WindowsFormsHost.new
+  host.child = control
+  w.Content = host
+  w
+end
+
+
